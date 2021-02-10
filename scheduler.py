8ef69a918039e9567de514f2649d6e98913575c0
Jiří Vlasák <jirivlasak9@gmail.com>
2021-02-10 11:13:55 +0100

    Added time parsing

--- scheduler.py
+++ scheduler.py
@@ -1,16 +1,30 @@
 import yaml
+import datetime
 from dateutil.parser import parse
+from pytimeparse.timeparse import timeparse
 
 
 with open("config.yaml", 'r') as stream:
     try:
         config = yaml.safe_load(stream)
-        print(config)
-        for subject in config:
-            start = parse(subject["start"])
-            end = parse(subject["end"])
-            print(start.month)
-            print(end)
 
     except yaml.YAMLError as exc:
         print(exc)
+
+
+print(config)
+now = datetime.datetime.now()
+for subject in config:
+    name = subject["name"]
+    start = subject["start"]
+    duration = timeparse(subject["duration"])
+    period = datetime.timedelta(0, timeparse(subject["period"]))
+    end = start + datetime.timedelta(0, duration)
+    count = subject["count"]
+    path = subject["path"]
+
+    while start <= now and count > 0:
+        print(f"Downloading {name} {start} to {path}")
+        start += period
+        end += period
+        count -= 1
