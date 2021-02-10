df0c441f9460e95a069bddb15ac43dadc6e5ff6a
Jiri Vlasak <jirivlasak9@gmail.com>
2021-02-10 01:34:16 +0100

    Added venv

--- scheduler.py
+++ scheduler.py
@@ -1,8 +1,16 @@
 import yaml
+from dateutil.parser import parse
+
 
 with open("config.yaml", 'r') as stream:
     try:
         config = yaml.safe_load(stream)
         print(config)
+        for subject in config:
+            start = parse(subject["start"])
+            end = parse(subject["end"])
+            print(start.month)
+            print(end)
+
     except yaml.YAMLError as exc:
         print(exc)
